0e03ab0eb97cdc950e51e14ded3d318f43cbaaf3
Robin Dijoux <robin.dijoux@master.hes-so.ch>
2022-05-01 19:56:38 +0200

    add dijkstra

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import dijkstra, {
+  findPath,
+  findPathWithCost,
+} from '../node_modules/modern-dijkstra';
 
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    const graph = {
+      a: { b: 1, d: 1 },
+      b: { a: 1, c: 1, e: 1 },
+      c: { b: 1, f: 1 },
+      d: { a: 1, e: 1, g: 1 },
+      e: { b: 1, d: 1, f: 1, h: 1 },
+      f: { c: 1, e: 1, i: 1 },
+      g: { d: 1, h: 1 },
+      h: { e: 1, g: 1, i: 1 },
+      i: { f: 1, h: 1 },
+    };
+    const path = findPath(graph, 'a', 'i');
+    return path;
   }
 }
